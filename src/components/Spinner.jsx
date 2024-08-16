68e4571f66a9f26518160fa9b4bda7470be2e441
pratha-hadap <prathamhadap@gmail.com>
2024-08-16 20:01:32 +0530

    Fixed a bug in the login function

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import "./Spinner.css"
 
 const Spinner = () => {
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center h-[100vh] w-[100vw]'>
       <div className='spinner'></div>
     </div>
   )
